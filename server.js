78b7f7a2ebfccf5f28e2645568852622ad27f0c7
Lee Hodges <leehodges@gmail.com>
2020-09-14 12:49:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/tourof-heroes'));
+app.use(express.static('./dist/TourofHeroes'));
 
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'dist/tourof-heroes' }
+  res.sendFile('app.component.html', { root: 'dist/TourofHeroes' }
   );
 });
 
