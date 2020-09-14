13fe6cb7eb3482cde9edc79f65d53ca6ea5887d5
Lee Hodges <leehodges@gmail.com>
2020-09-14 12:39:35 -0500

    s

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/tourof-heroes'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/tourof-heroes/index.html'));});
+
+app.use(express.static('./dist/tourof-heroes'));
+
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/tourof-heroes' }
+  );
+});
+
 app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
