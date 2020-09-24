c32dabc7d5dba3486dea35a6ac38b2602607ffce
Lee Hodges <leehodges@gmail.com>
2020-09-24 15:14:58 -0500

    xz
    xz

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import {Hero} from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Dr Nice', superpower: 'heart'},
+  { id: 12, name: 'Narco', superpower: 'earth' },
+  { id: 13, name: 'Bombasto', superpower: 'wind' },
+  { id: 14, name: 'Celeritas', superpower: 'fire' },
+  { id: 15, name: 'Magneta', superpower: 'evil' },
+  { id: 16, name: 'RubberMan', superpower: 'evil' },
+  { id: 17, name: 'Dynama', superpower: 'evil' },
+  { id: 18, name: 'Dr IQ', superpower: 'evil' },
+  { id: 19, name: 'Magma', superpower: 'evil' },
+  { id: 20, name: 'Tornado', superpower: 'evil' }
 ]
